1d6b857c01ab74cc3ad952a9eefb6595f086d7c2
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-06-20 14:01:32 +0930

    Adding DBContext class, EFCore Design

--- Persistence/VehicleManagementDbContext.cs
+++ Persistence/VehicleManagementDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace VehicleManagement.Persistence
 {
-    public class VehicleManagementDbContext
+    public class VehicleManagementDbContext : DbContext
     {
+        public VehicleManagementDbContext(DbContextOptions<VehicleManagementDbContext> options) 
+            : base(options)
+        {
+
+        }
     }
 }
