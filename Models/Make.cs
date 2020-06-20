fc56835030a39fd1851a1f0362bbdc9af9bb4ef3
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-06-20 13:19:59 +0930

    Adding the Domain Models
    Make
    Model

---
+++ Models/Make.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleManagement.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleManagement.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
