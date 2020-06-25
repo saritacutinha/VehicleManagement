ee400c6dde69e1a5c0edcbfd7c1d01eff39a99ed
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-06-25 14:55:02 +0930

    Adding Repository

---
+++ Persistence/VehicleRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleManagement.Models;
+
+namespace VehicleManagement.Persistence
+{
+    public class VehicleRepository
+    {
+        private readonly VehicleManagementDbContext context;
+
+        public VehicleRepository(VehicleManagementDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Vehicle>GetVehicle(int id)=>
+        
+            await context.Vehicles
+                           .Include(v => v.Type)
+                           .Include(v => v.Model)
+                          .ThenInclude(v => v.Make)
+                           .SingleOrDefaultAsync(v => v.Id == id);
+
+        
+    }
+}
